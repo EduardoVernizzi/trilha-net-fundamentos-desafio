dbc96a33428610e238291dccf1a2cca34c4bddc7
Eduardo Vernizzi <eduardo.vernizzi@hotmail.com>
2024-10-09 21:07:53 -0300

    update

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -73,7 +73,7 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+            
 
                 foreach (var veiculo in veiculos)
                 {
